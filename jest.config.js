968daf45a22977e23ad09c5a0781d229d76bd4ec
Maxence POUTORD <maxence.poutord@gmail.com>
2017-09-27 09:29:00 +0100

    :hammer: switch to jest.config.js file + update dependencies (#9)
    * :hammer: change preprocessor + switch to jest.config.js file
    
    * :hammer: rollback preprocessor + update dependencies

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  verbose: false,
+  cache: true,
+  moduleFileExtensions: [
+    'js',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: '<rootDir>/tests/unit/coverage',
+  mapCoverage: true
+}
